b93e0aae6c20911a94fb86fd1a0723751333888d
Thomas Counsell <tom@eyfs.info>
2019-01-15 03:54:22 +0000

    No need for the failing test any more

--- grid/grid-test.rkt
+++ grid/grid-test.rkt
@@ -6,11 +6,6 @@
          (require rackunit
                   "sheet.rkt"
                   "sheet-eval.rkt")
-
-         (test-case "This is where tests will go"
-                    (check-equal? (+ 1 1) 2 "1+1=2")
-                    (check-equal? (+ 1 2) 2 "1+2=2")
-                    )
          
          (test-case "Tests of sheet-eval"
            (let ((example-sheet
