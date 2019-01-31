066725fa75725deab3dd28e92fb2d616bac535b3
Oliver Strickson <ostrickson@turing.ac.uk>
2019-01-31 15:17:39 +0000

    Add scoped names (based on nested define) and different 'define' forms; remove 'let'

--- stack-lang/main.rkt
+++ stack-lang/main.rkt
@@ -12,8 +12,6 @@
          (rename-out (*& *))
          (rename-out (/& /))
          (rename-out (expt& expt))
-         (rename-out (let& let))
-         (rename-out (let*& let*))
          (rename-out (define& define))
          (rename-out (if& if))
 
@@ -67,27 +65,34 @@
 
 
 ;; Stacks
-;;-----------------------------------------------------------------------
-;; A stack is a list of lists, of the form "(name expr val)" where
-;; name (: symbol?) is the name of the entry on the stack, expr (: list)
-;; is the expression used to compute it (which can refer to names
-;; deeper on the stack)
+;; -----------------------------------------------------------------------
+;; A stack is a list of lists, of the form "(name scope expr val)"
+;; where name (: symbol?) is the name of the entry on the stack, scope
+;; (: list?) is a list of any enclosing definitions, expr (: list) is
+;; the expression used to compute it (which can refer to names deeper
+;; on the stack) and val is the value of the result.
 ;;
 (define (make-stack)        (list))
 (define (stack-push v s)    (cons v s))
 (define (stack-peek name s) (assv name s))
 (define (stack-top s)       (car s))
 (define (name v)            (car v))
-(define (expr v)            (cadr v))
-(define (val  v)            (caddr v))
+(define (scope v)           (cadr v))
+(define (expr v)            (caddr v))
+(define (val  v)            (cadddr v))
+
+;; The stack language supports nested function definitions: the scope
+;; of a name is provided by the list of enclosing definitions
+;; (innermost first; the top-level scope is empty).
+(define current-scope (make-parameter '()))
 
 ;; stack-top-rename : symbol? stack? -> stack?
 ;;
 ;; Given a stack, return a copy with the name of the top item changed
-;; to new-id.
+;; to new-id and the scope set to the current scope.
 (define (stack-top-rename new-id stack)
   (let ((top (stack-top stack)))
-    (cons (cons new-id (cdr top)) (cdr stack))))
+    (cons (list* new-id (current-scope) (cddr top)) (cdr stack))))
 
 ;; Pretty print stacks
 ;;
@@ -101,22 +106,25 @@
         (string-join (map format-elt (vector->list a)) " ")
         (format-elt a))))
 
-(define-syntax-rule (stack-print stack)
-  (let* ((name-fmt (map (compose ~a name) stack))
-         (val-fmt  (map (compose vector-format val) stack))
-         (expr-fmt (map (compose ~a expr) stack))
-         (name-width (apply max (map string-length name-fmt)))
-         (val-width  (apply max (map string-length val-fmt)))
-         (expr-width (apply max (map string-length expr-fmt))))
+(define (stack-print stack)
+  (let* ((scope-fmt   (map (compose ~a scope) stack))
+         (name-fmt    (map (compose ~a name) stack))
+         (val-fmt     (map (compose vector-format val) stack))
+         (expr-fmt    (map (compose ~a expr) stack))
+         (scope-width (apply max (map string-length scope-fmt)))
+         (name-width  (apply max (map string-length name-fmt)))
+         (val-width   (apply max (map string-length val-fmt)))
+         (expr-width  (apply max (map string-length expr-fmt))))
     (display
-     (foldl (lambda (n v e acc)
+     (foldl (lambda (s n v e acc)
               (string-append
-               (format "~a | ~a | ~a~%"
+               (format "~a ~a | ~a | ~a~%"
+                       (~a s #:min-width scope-width)
                        (~a n #:min-width name-width)
                        (~a e #:min-width expr-width)
                        (~a v #:min-width val-width))
                acc))
-           "" name-fmt val-fmt expr-fmt))))
+            "" scope-fmt name-fmt val-fmt expr-fmt))))
 
 
 ;; Datum
@@ -128,23 +136,24 @@
   (syntax-case stx ()
     [(_ . d)
      (with-syntax ([(name) (generate-temporaries '(|%e|))])
-       #'(stack-push (list 'name (#%datum . d) (#%datum . d))
+       #'(stack-push (list 'name (current-scope) (#%datum . d) (#%datum . d))
                      (make-stack)))]))
 
 
 ;; Defining stack functions
 ;;----------------------------------------------------------------------
 
-;; (define-stack-fn (f args ...) (f-str f-symb) body ...)
+;; (define-primitive-stack-fn (f args ...) (f-str f-symb) body ...)
 ;;
-;; Macro that expands to a definition of a function named "f", whose
-;; implementation is given by the racket forms body, and whose results
-;; are labelled on the stack by symbols constructed from "f-str" and a
-;; number (with a counter kept internally).  These names are interned,
-;; so the name provided must be unique amoung all such stack function
-;; definitions.
+;; Helper macro for defining the stack functions provided by this
+;; module.  It expands to a definition of a function named "f", whose
+;; implementation is given by the racket forms "body", and whose
+;; results are labelled on the stack by symbols constructed from
+;; "f-str" and a number (with a counter kept internally).  These names
+;; are interned, so the name provided must be unique amoung all such
+;; stack function definitions.
 ;;
-(define-syntax (define-stack-fn stx)
+(define-syntax (define-primitive-stack-fn stx)
   (syntax-case stx ()
     ;; f        : id?
     ;; args ... : stack? ...
@@ -169,9 +178,40 @@
                                    (append rargs ...)))
                       (res-name (make-name f-str (post-inc! name-counter))))
                  (stack-push (list res-name
+                                   (current-scope)
                                    (quasiquote (f-symb (unquote arg-names) ...))
                                    result)
                              args-stack))))))]))
+
+
+;; Like define-primitive-stack-fn, but the function body is assumed to
+;; already be in terms of stack functions (that is, accepting stacks
+;; as arguments and returning a stack).  Names of these functions do
+;; not appear on the expression stack (although their primitive
+;; subexpressions do).  This is a helper for implementing "define" in
+;; the language.
+;;
+(define-syntax (define-stack-fn stx)
+  (syntax-case stx ()
+    ;; f        : id?
+    ;; args ... : stack? ...
+    ;; body ... : expression? ...
+    [(_ (f args ...) body ...)
+     (with-syntax ([(rargs ...) (syntax-reverse #'(args ...))]
+                   [f-str #'(symbol->string 'f)])
+       #'(define f
+           (let ((name-counter 0))
+             (lambda (args ...)
+               (let* ((result-stack
+                       (parameterize ((current-scope (cons 'f (current-scope))))
+                         body ...))
+                      (result-val (val (stack-top result-stack)))
+                      (args-stack (append rargs ...))
+                      (res-name (make-name f-str (post-inc! name-counter))))
+                 (remove-duplicates-before
+                  (append
+                   (stack-top-rename res-name result-stack)
+                   args-stack)))))))]))
 
 ;; Given a function fn of two variables, return a function taking
 ;; either scalar or vector arguments and broadcasting any scalar
@@ -194,64 +234,69 @@
 ;; Stack operator definitions
 ;;-----------------------------------------------------------------------
 
-;; let should cause the value at the top of the stack returned by expr
-;; to be renamed to id.
+;; (define id expr)
+;; (define (id args ...) body ...)
 ;;
-(define-syntax-rule (let& ((id expr) ...) body ...)
-  (let ((id (stack-top-rename 'id expr)) ...)
-    body ...))
-(define-syntax-rule (let*& ((id expr) ...) body ...)
-  (let* ((id (stack-top-rename 'id expr)) ...)
-    body ...))
+;; The first form causes the result of evaluating expr to be bound by
+;; id, and in addition renames the result at the top of the stack to
+;; 'id.
+;;
+;; The second form defines a function taking the stack arguments args
+;; and returning the result of evaluating body.
+;;
+(define-syntax (define& stx)
+  (syntax-case stx ()
+    [(_ id expr)
+     #'(define id (stack-top-rename 'id expr))]
 
-(define-syntax-rule (define& id expr)
-  (define id (let& ((id expr)) id)))
+    [(_ (id args ...) body ...)
+     #'(define-stack-fn (id args ...) body ...)]))
 
-;; if is a function 
-(define-stack-fn (if& test-expr then-expr else-expr)
+;; if is a function
+(define-primitive-stack-fn (if& test-expr then-expr else-expr)
   ("branch" if)
   (if test-expr then-expr else-expr))
 
-(define-stack-fn (+& a b)
+(define-primitive-stack-fn (+& a b)
   ("sum" +) ;; results show up on the stack with names like "sum0"
             ;; and with expressions in terms of "+"
   ((vectorize +) a b))
 
-(define-stack-fn (-& a b)
+(define-primitive-stack-fn (-& a b)
   ("diff" -)
   ((vectorize -) a b))
 
-(define-stack-fn (*& a b)
+(define-primitive-stack-fn (*& a b)
   ("prod" *)
   ((vectorize *) a b))
 
-(define-stack-fn (/& a b)
+(define-primitive-stack-fn (/& a b)
   ("quot" /)
   ((vectorize /) a b))
 
-(define-stack-fn (expt& a b)
+(define-primitive-stack-fn (expt& a b)
   ("expn" expt)
   ((vectorize expt) a b))
 
-(define-stack-fn (sum xs)
+(define-primitive-stack-fn (sum xs)
   ("vsum" sum)
   (if (vector? xs)
       (sequence-fold + 0 xs)
       xs))
 
-(define-stack-fn (product xs)
+(define-primitive-stack-fn (product xs)
   ("vprod" product)
   (if (vector? xs)
       (sequence-fold * 0 xs)
       xs))
 
-(define-stack-fn (nth n a)
+(define-primitive-stack-fn (nth n a)
   ("n" nth)
   (if (vector? a)
       (vector-ref a n)
-      a)) ;; "broadcast" scalars to whatever we ask for
+      a))
 
-(define-stack-fn (len a)
+(define-primitive-stack-fn (len a)
   ("l" len)
   (if (vector? a)
       (vector-length a)
