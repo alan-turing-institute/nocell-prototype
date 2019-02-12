ac1e9f077e13b7a793916150092fb89d2a463eac
Oliver Strickson <ostrickson@turing.ac.uk>
2019-02-12 22:41:14 +0000

    Use grid in stack->grid

--- stack-lang/cell.rkt
+++ stack-lang/cell.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require "main.rkt"
+         "../grid/grid.rkt"
          math/array)
 
 (module+ test (require rackunit))
@@ -19,15 +20,14 @@
   (let ((op         (caar expr))
         (arg-dims   (cdar expr))
         (arg-names  (cdr  expr)))
-    (list* 'cell-expr op
+    (list* op
            (map (lambda (dim name)
-                  (list 'cell-ref
-                        (make-name name (if (null? dim) null idx))))
+                  (ref (make-name name (if (null? dim) null idx))))
                 arg-dims arg-names))))
 
 (module+ test
   (check-equal? (unwrap-binary-op-expr '([+ () (3)] %e1 %e2) '(2))
-                '(cell-expr + (cell-ref "%e1") (cell-ref "%e2[2]"))))
+                (list '+ (ref "%e1") (ref "%e2[2]"))))
 
 (define (expr-op expr)
   (match expr
@@ -52,16 +52,16 @@
            (let* ((args           (cdr expr))
                   (arg-dims       (cdar expr))
                   (args-unwrapped (car (map unwrap args arg-dims))))
-             (list* 'row
-                    (list 'cell
-                          (foldl
-                           (lambda (x acc)
-                             (list 'cell-expr '+ acc (list 'cell-ref x)))
-                           (list 'cell-ref (car args-unwrapped))
-                           (cdr args-unwrapped)))))]
+             (row
+              (cell
+               (foldl
+                (lambda (x acc)
+                  (list '+ acc (ref x)))
+                (ref (car args-unwrapped))
+                (cdr args-unwrapped)))))]
 
           [else ;; values, refs or vectorized builtin eval
-           (list* 'row
+           (apply row
                   (for/list ([v (in-vector vector-vals)]
                              [col (in-naturals)])
                     (let* ((idx (if (null? (shape vals))
@@ -72,60 +72,67 @@
                               [(equal? expr vals) ;; expr is a value
                                (vector-ref vector-vals col)]
                               [(symbol? expr)
-                               (list 'cell-ref (make-name expr idx))]
+                               (ref (make-name expr idx))]
                               [else
                                (unwrap-binary-op-expr expr idx)])))
-                      (list 'cell cell-expr
-                            '#:id (make-name (assignment-id a) idx)))))]
+                      (cell cell-expr
+                            #:id (make-name (assignment-id a) idx)))))]
           )))
 
 (module+ test
-  (let ((expected '(row (cell 1 #:id "%e1")))
+  (let ((expected (row (cell 1 #:id "%e1")))
         (actual   (assignment->row
                    (assignment '%e1 '() '() 1 1))))
     (check-equal? actual expected))
 
-  (let ((expected '(row (cell 3 #:id "%e1[0]")
-                        (cell 1 #:id "%e1[1]")
-                        (cell 4 #:id "%e1[2]")))
+  (let ((expected (row (cell 3 #:id "%e1[0]")
+                       (cell 1 #:id "%e1[1]")
+                       (cell 4 #:id "%e1[2]")))
         (actual   (assignment->row
                    (assignment '%e1 '() '() #(3 1 4) #(3 1 4)))))
     (check-equal? actual expected))
 
-  (let ((expected '(row (cell (cell-expr + (cell-ref "%e1[0]") (cell-ref "%e2"))
-                                         #:id "%sum1[0]")
-                        (cell (cell-expr + (cell-ref "%e1[1]") (cell-ref "%e2"))
-                                         #:id "%sum1[1]")
-                        (cell (cell-expr + (cell-ref "%e1[2]") (cell-ref "%e2"))
-                                         #:id "%sum1[2]")))
+  (let ((expected (row (cell (list '+ (ref "%e1[0]") (ref "%e2"))
+                             #:id "%sum1[0]")
+                       (cell (list '+ (ref "%e1[1]") (ref "%e2"))
+                             #:id "%sum1[1]")
+                       (cell (list '+ (ref "%e1[2]") (ref "%e2"))
+                             #:id "%sum1[2]")))
         (actual (assignment->row
                  (assignment '%sum1 '() '() '([+ (3) ()] %e1 %e2) #(0 1 2)))))
     (check-equal? actual expected))
 
-  (let ((expected '(row (cell (cell-ref "target") #:id "a")))
+  (let ((expected (row (cell (ref "target") #:id "a")))
         (actual   (assignment->row
                    (assignment 'a '() '() 'target 0))))
     (check-equal? actual expected))
 
   ;; "sum" results in a fold
-  (let ((expected '(row
-                    cell
-                    (cell-expr
-                     +
-                     (cell-expr + (cell-ref "a[0]") (cell-ref "a[1]"))
-                     (cell-ref "a[2]"))))
-        
+  (let ((expected (row (cell (list '+ (list '+ (ref "a[0]") (ref "a[1]"))
+                                   (ref "a[2]")))))
         (actual   (assignment->row
                    (assignment 'result '() '() '([sum (3)] a) 0))))
     (check-equal? actual expected)))
+  
 
-;; ;; assignment-list->sheet :: (List assignment?) -> sheet?
-;; (define (assignments->sheet a)
-;;   (let loop ((a a)
-;;              (rows (list)))
-;;     (if (null? a)
-;;         rows
-;;         (loop (cdr a)
-;;               (cons (assignment->row (car a)) rows)))
-;;     ))
+;; assignments->sheet :: (List assignment?) -> sheet?
+(define (assignments->sheet a)
+  (apply sheet (map assignment->row (reverse a))))
+
+(module+ test
+  (let ((expected (sheet (row (cell  0 #:id "a[0]")
+                              (cell -1 #:id "a[1]")
+                              (cell -2 #:id "a[2]"))
+                         (row (cell  0 #:id "b[0]")
+                              (cell  2 #:id "b[1]")
+                              (cell  4 #:id "b[2]"))
+                         (row (cell (list '+ (ref "a[0]") (ref "b[0]")))
+                              (cell (list '+ (ref "a[1]") (ref "b[1]")))
+                              (cell (list '+ (ref "a[2]") (ref "b[2]"))))))
+        (actual   (assignments->sheet
+                   (list
+                    (assignment 'result '() '() '([+ (3) (3)] a b) #(0 1 2))
+                    (assignment 'b      '() '() #(0  2  4) #(0  2  4))
+                    (assignment 'a      '() '() #(0 -1 -2) #(0 -1 -2))))))
+    (check-equal? actual expected)))
 
