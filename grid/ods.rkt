7c5d03eae2b9849eff9f89767918cb85aff2c93f
James Geddes <jgeddes@turing.ac.uk>
2019-03-01 16:02:16 +0000

    Working on ods

--- grid/ods.rkt
+++ grid/ods.rkt
@@ -5,14 +5,19 @@ Convert a single `sheet` to a bytestream representation of an OpenDocument Sprea
 Part 1, 2.2.4]. 
 
 [odf] Open Document Format for Office Applications (OpenDocument) Version 1.2 
+
+TODO and LIMITATIONS
+ - Doesn't cope with named ranges
+
 |#
+
+
 
 ;; ---------------------------------------------------------------------------------------------------
 
 (require racket/contract
          math/array
          sxml
-         file/zip
          "sheet.rkt"
          "openformula.rkt")
 
@@ -22,75 +27,93 @@ Part 1, 2.2.4].
     (table:table-column)
     (table:table-row
      (table:table-cell
-      (@ (office:value= "42"))
+      (@ (office:value "42")
+         (office:value-type "float"))
       (text:p 42)))))
 
-;; provides:
-;; - sheet-odf : sheet -> xml?
-;; - ods-sheet : sheet -> bytestream
-
-
-#| 
-An OpenDocument package is a zip file containing at least the files;
-- mimetype
-- content.xml
-|#
-
-(define MIMETYPE "application/vnd.oasis.opendocument.spreadsheet")
-
-
-
+(define *MIMETYPE* "application/vnd.oasis.opendocument.spreadsheet")
 
 ;; ---------------------------------------------------------------------------------------------------
 ;; Export sheet to a directory; zip the directory; and delete the original directory
 
-;; sheet? path? path-string? -> Void
+;; sheet? path-string? -> void
 ;; `path` should not be the name of an existing directory.  
-(define (sheet-export-ods sheet path filename)
-  (define package-directory (build-path path filename))
-  (when (directory-exists? package-directory)
-    (raise-user-error "Filename of output file (without extension) must not be the name of an existing directory"
-                      (path->string path)))
-  (make-directory package-directory)
-  (parameterize ([current-directory package-directory])
-    (write-ods-package sheet))
-  (zip-ods-package path filename))
-
-;; Write out the contents of the package to the current directory
-(define (write-ods-package sheet)
-  (call-with-output-file "content.xml"
-    (λ (out) (srl:sxml->xml (content sheet) out)))
-  (call-with-output-file "mimetype"
-    (λ (out) (write-string MIMETYPE out))))
-
-(define (zip-ods-package path filename)
-  (println "Done!"))
-
+(define (sheet-write-ods sheet path)
+   (call-with-output-file "content.xml"
+    (λ (out) (srl:sxml->xml (ods sheet) out))))
 
 ;; ---------------------------------------------------------------------------------------------------
-;; Wrap XML boilerplate for content.xml file around the exported sheet 
+;; Wrap XML boilerplate for the file around the exported sheet 
 
 ;; sheet? -> sxml?
-(define (content sheet)
+(define (ods sheet)
   `(*TOP*
     (@ (*NAMESPACES*
         [office "urn:oasis:names:tc:opendocument:xmlns:office:1.0"]
         [table  "urn:oasis:names:tc:opendocument:xmlns:table:1.0"]
         [text   "urn:oasis:names:tc:opendocument:xmlns:text:1.0"]))
     (*PI* xml "version=\"1.0\" encoding=\"UTF-8\"")
-    ,(document-content sheet)))
+    (office:document
+     (@ (office:version "1.2")
+        (office:mimetype ,*MIMETYPE*))
+     ,(ods-body sheet))))
 
-(define (document-content sheet)
-  `(office:document-content
-    (@ (office:version "1.2"))
-    ,(body sheet)))
-
-(define (body sheet)
+(define (ods-body sheet)
   `(office:body
-    ,(spreadsheet sheet)))
+    ,(ods-spreadsheet sheet)))
 
-(define (spreadsheet sheet)
-  `(office:spreadsheet ,PLACEHOLDER-TABLE))
+(define (ods-spreadsheet sheet)
+  `(office:spreadsheet
+    ,(ods-table sheet)))
+
+;; ---------------------------------------------------------------------------------------------------
+;; Emit an office:table version of a sheet
+
+;; TODO: Information about named ranges is lost after this point
+(define (ods-table sheet)
+  `(office:table
+    (@ (table:name ,(or (sheet-name sheet) "Sheet 1")))
+    (table:table-column) ; Why is this required?
+    ,(ods-rows (sheet-cells sheet))))
+
+(define (ods-rows sheet)
+  #f)
+
+(define (ods-row s)
+  #f)
+
+;; ods-cell : cell? integer? integer?
+(define (ods-cell c row col)
+  (let ([expr (cell-content cell)])
+    (cond
+      [(cell-value? expr) (ods-cell-value (cell-value-elements cell-value))]
+      [else #f])))
+
+(define (ods-cell-value elems)
+  (if (simple-cell-value? elems)
+      (let ([val (atomise elems)])
+        (cond
+          [(nothing? val) (ods-cell-empty)]
+          [(real? val)    (ods-cell-real val)]
+          [(string? val)  (ods-cell-string val)]
+          [(boolean? val) (ods-cell-boolean val)]))
+      #f))
+
+(define (ods-cell-empty)
+  '(table:table-cell))
+
+(define (ods-cell-real v)
+  '(table:table-cell ))
+
+(define (ods-cell-empty)
+  '(table:table-cell))
+
+(define (ods-cell-empty)
+  '(table:table-cell))
+
+
+
+
 
 
 
