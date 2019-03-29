65c0306c553ed3e51b81439f84adeda4ab284bc3
Oliver Strickson <ostrickson@turing.ac.uk>
2019-03-29 15:26:03 +0000

    tweaks

--- test/main.rkt
+++ test/main.rkt
@@ -23,22 +23,22 @@
   (define maybe-actual ((lift/maybe conv) maybe-input))
   ((lift/maybe check-equal?) maybe-actual maybe-expect))
 
-;; Check conversions nocell -> cell -> grid -> sxml -> fods
+;; Check conversions nocell -> cell -> grid -> sxml, grid -> fods
 ;;
 ;; nocell->cell just a case of evaluating `result`, done when we require it
 (define check:nocell->cell (check-convert identity))
 (define check:cell->grid   (check-convert stack->sheet))
 (define check:grid->sxml   (check-convert ods))
-(define check:sxml->fods   (check-convert srl:sxml->xml))
+(define check:grid->fods   (check-convert (curryr sheet-write-ods "out.fods")))
 
 (module+ test
   (for ([test-dir (directory-list "test-examples" #:build? #t)])
     (define (paths/suffix suffix) (filter (curryr path-has-extension? suffix)
                                           (directory-list test-dir #:build? #t)))
-    (match-let
-        ([(list nocell-path cell-path grid-path sxml-path fods-path)
-          (map paths/suffix '(".nocell" ".cell" ".grid" ".sxml" ".fods.rkt"))])
+    (match-let ([(list nocell-path cell-path grid-path sxml-path)
+                 (map paths/suffix '(".nocell" ".cell" ".grid" ".sxml"))])
       (check:nocell->cell nocell-path cell-path)
       (check:cell->grid   cell-path   grid-path)
       (check:grid->sxml   grid-path   sxml-path)
-      (check:sxml->fods   sxml-path   fods-path))))
+      ;; check that the conversion succeeds, but not the resulting file
+      (check:grid->fods   grid-path   null))))
