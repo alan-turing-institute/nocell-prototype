75f3301dec04064a5ab567886c790ec7841fe945
Oliver Strickson <ostrickson@turing.ac.uk>
2019-03-29 13:27:06 +0000

    Initial end-to-end test examples

---
+++ test/main.rkt
@@ -0,0 +1,44 @@
+#lang racket
+(require rackunit
+         sxml
+         "../stack-lang/main.rkt"
+         "../stack-lang/cell.rkt"
+         "../grid/ods.rkt")
+
+;; "maybe" implemented with lists
+;; nothing = null
+;; just a  = (list a)
+(define ((lift/maybe fn) . args)
+  (if (ormap null? args)
+      null
+      (list (apply fn (map car args)))))
+
+;; Check that conv correctly converts from the data read from
+;; maybe-input-path (if not missing) to the data read from
+;; maybe-expected-path.  If the expected path is missing, attempt the
+;; conversion anyway, even though the check cannot be performed.
+(define ((check-convert conv) maybe-input-path maybe-expect-path)
+  (define maybe-input  (map (curryr dynamic-require 'result) maybe-input-path))
+  (define maybe-expect (map (curryr dynamic-require 'result) maybe-expect-path))
+  (define maybe-actual ((lift/maybe conv) maybe-input))
+  ((lift/maybe check-equal?) maybe-actual maybe-expect))
+
+;; Check conversions nocell -> cell -> grid -> sxml -> fods
+;;
+;; nocell->cell just a case of evaluating `result`, done when we require it
+(define check:nocell->cell (check-convert identity))
+(define check:cell->grid   (check-convert stack->sheet))
+(define check:grid->sxml   (check-convert ods))
+(define check:sxml->fods   (check-convert srl:sxml->xml))
+
+(module+ test
+  (for ([test-dir (directory-list "test-examples" #:build? #t)])
+    (define (paths/suffix suffix) (filter (curryr path-has-extension? suffix)
+                                          (directory-list test-dir #:build? #t)))
+    (match-let
+        ([(list nocell-path cell-path grid-path sxml-path fods-path)
+          (map paths/suffix '(".nocell" ".cell" ".grid" ".sxml" ".fods.rkt"))])
+      (check:nocell->cell nocell-path cell-path)
+      (check:cell->grid   cell-path   grid-path)
+      (check:grid->sxml   grid-path   sxml-path)
+      (check:sxml->fods   sxml-path   fods-path))))
