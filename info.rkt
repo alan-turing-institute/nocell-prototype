f202499c7ec9ace46f3e57db776b60dcb5880b32
Oliver Strickson <ostrickson@turing.ac.uk>
2019-05-01 19:54:32 +0100

    Use github URL for gamble dep

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "nocell")
-(define deps '("base" "rackunit-lib" "parsack" "sxml" "gamble"))
+(define deps '("base" "rackunit-lib" "parsack" "sxml" "git://github.com/rmculpepper/gamble"))
 (define pkg-desc "A language for building probabilistic spreadsheets")
 (define version "0.0")
 (define pkg-authors '(ots22 tamc triangle-man))
