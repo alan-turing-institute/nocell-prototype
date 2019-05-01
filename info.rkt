65d17fb78e04618daa80f0c860e6f6516839dcb5
Oliver Strickson <ostrickson@turing.ac.uk>
2019-05-01 19:48:37 +0100

    Add gamble to deps (info)

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "nocell")
-(define deps '("base" "rackunit-lib" "parsack" "sxml"))
+(define deps '("base" "rackunit-lib" "parsack" "sxml" "gamble"))
 (define pkg-desc "A language for building probabilistic spreadsheets")
 (define version "0.0")
 (define pkg-authors '(ots22 tamc triangle-man))
